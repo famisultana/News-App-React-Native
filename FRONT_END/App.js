bddbcf8f118f51fbfa506fdd292b72c6ed7724f8
Fatima Sultana <fami.sultana16@gmail.com>
2019-09-21 00:07:38 +0500

    Horizontal list

--- FRONT_END/App.js
+++ FRONT_END/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import {Fragment} from 'react-native';
+import {ScrollView} from 'react-native';
 import Detailed_Article from './Screens/Detailed_Article';
+import Articles from './Screens/Articles';
 
 const App = () => {
-  return <Detailed_Article></Detailed_Article>;
+  return (
+    <ScrollView>
+      <Articles/>
+    <Detailed_Article/>
+    </ScrollView>
+  );
 };
 
 export default App;
